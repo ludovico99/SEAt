bb5471057ee3d35f91ccab8a4c90d47d6a92e09a
ludovico99 <ludovico.zarr@gmail.com>
2022-11-23 18:18:09 +0100

    ok

--- SEAT_Project/run.sh
+++ SEAT_Project/run.sh
@@ -8,17 +8,18 @@ export SCALE_FACTOR=3
 echo "SCALE_FACTOR=$SCALE_FACTOR" > config/.env
 
 
-GROUP="docker"
+# GROUP="docker"
 
-/bin/egrep  -i "^${GROUP}:" /etc/group
-if [ $? -eq 0 ]; then
-   echo "Group $GROUP exists in /etc/group"
-else 
-   echo "Group $GROUP does not exists in /etc/group"
-   sudo groupadd docker
-fi
+# /bin/egrep  -i "^${GROUP}:" /etc/group
+# if [ $? -eq 0 ]; then
+#    echo "Group $GROUP exists in /etc/group"
+# else 
+#    echo "Group $GROUP does not exists in /etc/group"
+#    sudo groupadd docker
+# fi
 
-sudo usermod -aG docker $USER
-sudo chmod +x /usr/local/bin/docker-compose
+# sudo usermod -aG docker $USER
+
+# sudo chmod +x /usr/local/bin/docker-compose
 
 docker-compose --env-file=./config/.env up --scale payment=$SCALE_FACTOR --build
