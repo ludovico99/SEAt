6927f2f3d9be738795ecac66833d39313bcafce0
elisavenditti <elisavenditti99@gmail.com>
2022-11-26 13:31:10 +0100

    aggiornate alcune cose mancanti

--- SEAT_Project/deploy.sh
+++ SEAT_Project/deploy.sh
@@ -6,7 +6,7 @@ IPADDR=$( cat $PROJ_DIR/hosts.ini | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" )
 
 sudo scp -v -i $SSH -r $PROJ_DIR ubuntu@$IPADDR:/home/ubuntu
 
-sudo scp -v -i $SSH -r $HOME/.aws/ ubuntu@$IPADDR:/home/ubuntu/.aws/
+sudo scp -v -i $SSH -r $HOME/.aws/ ubuntu@$IPADDR:/root/.aws/
 
 # sudo scp -v -i $SSH -r $PROJ_DIR/run.sh ubuntu@$IPADDR:/home/ubuntu/SEAT_Project/run.sh
 cd $PROJ_DIR
