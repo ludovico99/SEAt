72f3f0923deb3b173a5f6c41bb1548c1b88fa239
elisavenditti <elisavenditti99@gmail.com>
2022-11-26 20:04:17 +0100

    risolto retrieve delle credenziali da istanza ec2

--- SEAT_Project/deploy.sh
+++ SEAT_Project/deploy.sh
@@ -6,9 +6,10 @@ IPADDR=$( cat $PROJ_DIR/hosts.ini | grep -E -o "([0-9]{1,3}[\.]){3}[0-9]{1,3}" )
 
 sudo scp -v -i $SSH -r $PROJ_DIR ubuntu@$IPADDR:/home/ubuntu
 
-sudo scp -v -i $SSH -r $HOME/.aws/ ubuntu@$IPADDR:/root/.aws/
+sudo scp -v -i $SSH -r $HOME/.aws/ ubuntu@$IPADDR:/home/ubuntu/.aws/
 
 # sudo scp -v -i $SSH -r $PROJ_DIR/run.sh ubuntu@$IPADDR:/home/ubuntu/SEAT_Project/run.sh
+
 cd $PROJ_DIR
 
 ansible-playbook -v -i hosts.ini deploy.yaml
--- SEAT_Project/run.sh
+++ SEAT_Project/run.sh
@@ -19,7 +19,7 @@ echo "SCALE_FACTOR=$SCALE_FACTOR" > config/.env
 # fi
 
 # sudo usermod -aG docker $USER
-
+cp /home/ubuntu/.aws/credentials /root/.aws/
 sudo chmod +x /usr/local/bin/docker-compose
 
 docker-compose --env-file=./config/.env up --scale payment=$SCALE_FACTOR --build
